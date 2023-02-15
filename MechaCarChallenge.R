512171f5d2d6cb8e4bebd69d14e9c720ce41e01a
omnom1997 <rackley.lily@gmail.com>
2023-02-15 13:38:18 -0700

    Completed Part 2 of challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,9 @@
 library(dplyr)
 mpg_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors=F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_table)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_table))
+
+suspension <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+total_summary <- suspension %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
